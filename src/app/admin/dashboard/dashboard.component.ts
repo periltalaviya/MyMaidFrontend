11a7de9924eff93771a7d06cde5398e14afa6e8c
Peril <periltalaviya123@gmail.com>
2022-03-16 17:32:11 +0530

    counter added

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BookingService } from 'src/app/service/booking.service';
+import { CategoryService } from 'src/app/service/category.service';
+import { ClientService } from 'src/app/service/client.service';
+import { MaidService } from 'src/app/service/maid.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  maidCounter: number = 0
+  clientCounter: number = 0
+  bookingCounter: number = 0
+  categoryCounter: number = 0
 
-  constructor() { }
+  constructor(private maidService: MaidService, private clientService: ClientService,
+    private bookingService: BookingService, private categoryService: CategoryService) { }
 
   ngOnInit(): void {
+    this.maidService.getMaid().then(res =>{
+      this.maidCounter = res.length    
+    })
+
+    this.clientService.getClient().then(res =>{
+      this.clientCounter = res.length    
+    })
+
+    this.bookingService.getBooking().then(res =>{
+      this.bookingCounter = res.length    
+    })
+
+    this.categoryService.getCategory().then(res =>{
+      this.categoryCounter = res.length
+    })
   }
 
 }
